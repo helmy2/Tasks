286eddcafd50ac4ffd800780af79f3bcb7656751
helmy <mohamed0132525715@gmail.com>
2021-12-29 16:35:35 +0200

    change addTaskScreen

--- app/src/main/java/com/example/tasks/presentation/addTask/components/AddTaskField.kt
+++ app/src/main/java/com/example/tasks/presentation/addTask/components/AddTaskField.kt
@@ -1,14 +1,13 @@
 package com.example.tasks.presentation.addTask.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,8 +19,8 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.tasks.domain.model.Task
 import com.example.tasks.domain.model.TaskList
-import com.example.tasks.presentation.util.toColor
 import com.example.tasks.domain.util.getCurrentDate
+import com.example.tasks.presentation.util.toColor
 
 @Composable
 fun AddTaskField(
@@ -34,15 +33,20 @@ fun AddTaskField(
     val focusManager = LocalFocusManager.current
 
     var taskName by remember { mutableStateOf(task?.title ?: "") }
-    var taskDescription by remember { mutableStateOf(task?.description ?:"") }
+    var taskDescription by remember { mutableStateOf(task?.description ?: "") }
     var selectedIndex by remember { mutableStateOf(0) }
     var date by remember { mutableStateOf(task?.date ?: getCurrentDate()) }
+
+    LaunchedEffect(key1 = true) {
+        if (list.isNotEmpty() && task != null)
+            selectedIndex = list.indexOfFirst { it.id == task.listId }
+    }
 
     Column(
         Modifier
             .padding(16.dp)
+            .padding(horizontal = 32.dp)
             .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
 
@@ -62,6 +66,13 @@ fun AddTaskField(
                 modifier = Modifier.align(Alignment.Center),
             )
         }
+
+        DropdownField(
+            list,
+            selectedIndex,
+            onItemClick = { selectedIndex = it },
+            modifier = Modifier.padding(top = 8.dp)
+        )
 
         OutlinedTextField(
             value = taskName,
@@ -83,28 +94,58 @@ fun AddTaskField(
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
         )
 
-        LazyRow {
-            itemsIndexed(list) { index, task ->
-                TextButton(
-                    shape = RoundedCornerShape(16.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = if (index == selectedIndex) task.color.toColor() else task.color.toColor()
-                            .copy(alpha = .3f)
-                    ),
-                    onClick = {
-                        selectedIndex = index
-                    },
-                    modifier = Modifier.padding(horizontal = 8.dp)
-                ) {
-                    Text(text = task.title)
-                }
-            }
-        }
-        DateField(context, onDateChange = { date = it },currantDate = date)
+        DateField(context, onDateChange = { date = it }, currantDate = date)
         Button(onClick = {
             onItemClick(taskName, taskDescription, list[selectedIndex].id!!, date)
-        }) {
+        }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
             Text(text = "Save")
         }
     }
 }
+
+@Composable
+private fun DropdownField(
+    list: List<TaskList>,
+    selectedIndex: Int,
+    onItemClick: (index: Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
+    if (list.isNotEmpty())
+        Box(modifier) {
+
+            Row(modifier = Modifier
+                .padding(8.dp)
+                .clickable(onClick = { expanded = true })
+            ) {
+                Icon(
+                    imageVector = Icons.Default.List,
+                    contentDescription = "",
+                    tint = list[selectedIndex].color.toColor().copy(alpha = .9f)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(list[selectedIndex].title)
+            }
+
+
+            DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                list.forEachIndexed { index, taskList ->
+                    DropdownMenuItem(onClick = {
+                        onItemClick(index)
+                        expanded = false
+                    }) {
+                        Row(modifier = Modifier.padding(8.dp)) {
+                            Icon(
+                                imageVector = Icons.Default.List,
+                                contentDescription = "",
+                                tint = taskList.color.toColor().copy(alpha = .9f)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(taskList.title)
+                        }
+                    }
+                }
+            }
+        }
+}
+
--- app/src/main/java/com/example/tasks/presentation/addTask/components/DateField.kt
+++ app/src/main/java/com/example/tasks/presentation/addTask/components/DateField.kt
@@ -4,50 +4,85 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Context
 import android.widget.DatePicker
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Timer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.tasks.domain.util.convertLongToTime
+import java.text.SimpleDateFormat
 import java.util.*
 
 @Composable
 fun DateField(context: Context, onDateChange: (date: Long) -> Unit, currantDate: Long) {
-    val now = Calendar.getInstance()
-    now.timeInMillis = currantDate
+    val calendar = Calendar.getInstance()
+    calendar.timeInMillis = currantDate
 
-    val date = remember { mutableStateOf(now.timeInMillis.convertLongToTime()) }
+    val locale = Locale.getDefault()
+    val dateFormatter = SimpleDateFormat("yyyy.MM.dd", locale)
+    val timeFormatter = SimpleDateFormat("h:mm a", locale)
 
     val timePickerDialog = TimePickerDialog(
         context,
         { _, hour, minute ->
-            now[Calendar.HOUR_OF_DAY] = hour
-            now[Calendar.MINUTE] = minute
+            calendar[Calendar.HOUR_OF_DAY] = hour
+            calendar[Calendar.MINUTE] = minute
 
-            date.value = now.timeInMillis.convertLongToTime()
-        }, now[Calendar.HOUR_OF_DAY], now[Calendar.MINUTE], false
+            onDateChange(calendar.timeInMillis)
+        }, calendar[Calendar.HOUR_OF_DAY], calendar[Calendar.MINUTE], false
     )
-
 
     val datePickerDialog = DatePickerDialog(
         context, { _: DatePicker, year: Int, month: Int, day: Int ->
-            now[Calendar.YEAR] = year
-            now[Calendar.MONTH] = month
-            now[Calendar.DAY_OF_MONTH] = day
+            calendar[Calendar.YEAR] = year
+            calendar[Calendar.MONTH] = month
+            calendar[Calendar.DAY_OF_MONTH] = day
 
-            onDateChange(now.timeInMillis)
-
-            timePickerDialog.show()
-            date.value = now.timeInMillis.convertLongToTime()
-
-        }, now[Calendar.YEAR], now[Calendar.MONTH], now[Calendar.DAY_OF_MONTH]
+            onDateChange(calendar.timeInMillis)
+        }, calendar[Calendar.YEAR], calendar[Calendar.MONTH], calendar[Calendar.DAY_OF_MONTH]
     )
 
+    Column {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+                datePickerDialog.show()
+            }) {
+            Icon(
+                imageVector = Icons.Default.DateRange,
+                contentDescription = "",
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("Date")
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(text = dateFormatter.format(calendar.timeInMillis))
+        }
 
-    Button(onClick = {
-        datePickerDialog.show()
-    }) {
-        Text(text = "Selected date: ${date.value}")
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+                timePickerDialog.show()
+            }) {
+            Icon(
+                imageVector = Icons.Default.Timer,
+                contentDescription = "",
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("Time")
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(text = timeFormatter.format(calendar.timeInMillis))
+        }
     }
 }
